3d707999cf62d83d87ccea22d3e63d2b20a32288
Tameem Safi <t.safi@kainos.com>
2017-11-09 14:08:42 +0000

    Change dom ready event for  turbolinks

--- src/assets/js/development/index.js
+++ src/assets/js/development/index.js
@@ -4,11 +4,21 @@ import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 
+import Turbolinks from 'turbolinks';
+
 import { LibraryPageNavigation } from './components/library-page-navigation';
 import { TurbolinksIntegration } from './components/turbolinks-integration';
 import { domReady } from './../shared/misc';
 
-domReady(function() {
+const readyFunction = (fn) => {
+  if( Turbolinks.supported ) {
+    document.addEventListener("turbolinks:load", fn);
+  } else {
+    domReady(fn);
+  }
+};
+
+readyFunction(function() {
 
   new LibraryPageNavigation();
   new TurbolinksIntegration();

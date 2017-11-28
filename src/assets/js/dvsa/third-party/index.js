576bb49840703eddb3c91307b614fdc93578be94
Tameem Safi <tameem@safi.me.uk>
2017-11-28 11:23:46 +0000

    Added promise polyfill

--- src/assets/js/dvsa/third-party/index.js
+++ src/assets/js/dvsa/third-party/index.js
@@ -1,6 +1,8 @@
 import './details-polyfill';
 import 'core-js/fn/array/from';
 import 'core-js/fn/array/for-each';
+import 'core-js//es6/promise';
+import 'core-js//fn/promise';
 import 'element-closest';
 import 'blissfuljs';
 

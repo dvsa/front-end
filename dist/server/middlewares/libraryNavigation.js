dac31a88d60328ba5d2d4516478c7ee3be35ac9c
Tameem Safi <tameem@safi.me.uk>
2017-12-05 12:49:45 +0000

    Build production for v1.0.5

--- dist/server/middlewares/libraryNavigation.js
+++ dist/server/middlewares/libraryNavigation.js
@@ -23,75 +23,89 @@ var _constants = require('./../config/constants');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 const isDirectory = exports.isDirectory = source => _fs2.default.lstatSync(source).isDirectory();
 
 const getDirectories = exports.getDirectories = source => {
   return _fs2.default.readdirSync(source).map(name => _path2.default.join(source, name)).filter(isDirectory);
 };
 
-const getAllFilePathsWithinPath = exports.getAllFilePathsWithinPath = async path => {
-  // Creates a promise since the function uses a callback
-  return new Promise((resolve, reject) => {
-    _nodeDir2.default.paths(path, (err, paths) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(paths.files);
+const getAllFilePathsWithinPath = exports.getAllFilePathsWithinPath = (() => {
+  var _ref = _asyncToGenerator(function* (path) {
+    // Creates a promise since the function uses a callback
+    return new Promise(function (resolve, reject) {
+      _nodeDir2.default.paths(path, function (err, paths) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(paths.files);
+      });
     });
   });
-};
 
-const addLibraryNavigationItemsToRequestObject = exports.addLibraryNavigationItemsToRequestObject = async (req, res, next) => {
-  let currentRoute = req.path;
-  let parentRoute = currentRoute.replace('/' + _path2.default.basename(req.path), '');
+  return function getAllFilePathsWithinPath(_x) {
+    return _ref.apply(this, arguments);
+  };
+})();
 
-  let libraryFolderPaths = getDirectories(_path2.default.join(_constants.CONFIG.paths.views.base, 'library'));
-  let navigationItems = [];
+const addLibraryNavigationItemsToRequestObject = exports.addLibraryNavigationItemsToRequestObject = (() => {
+  var _ref2 = _asyncToGenerator(function* (req, res, next) {
+    let currentRoute = req.path;
+    let parentRoute = currentRoute.replace('/' + _path2.default.basename(req.path), '');
 
-  for (let i = 0; i < libraryFolderPaths.length; i++) {
-    let currentPath = libraryFolderPaths[i];
-    let foldername = _path2.default.basename(currentPath);
-    let fileRouteName = currentPath.replace(_constants.CONFIG.paths.views.base, '');
-    let filePathsWithinCurrentFolder = await getAllFilePathsWithinPath(currentPath);
-    let subItems = [];
+    let libraryFolderPaths = getDirectories(_path2.default.join(_constants.CONFIG.paths.views.base, 'library'));
+    let navigationItems = [];
 
-    let parentActive = false;
+    for (let i = 0; i < libraryFolderPaths.length; i++) {
+      let currentPath = libraryFolderPaths[i];
+      let foldername = _path2.default.basename(currentPath);
+      let fileRouteName = currentPath.replace(_constants.CONFIG.paths.views.base, '');
+      let filePathsWithinCurrentFolder = yield getAllFilePathsWithinPath(currentPath);
+      let subItems = [];
 
-    if (fileRouteName == req.path || fileRouteName + '/' == req.path || fileRouteName + '/index' == req.path) {
-      parentActive = true;
-    }
+      let parentActive = false;
 
-    for (let i = 0; i < filePathsWithinCurrentFolder.length; i++) {
-      let filepath = filePathsWithinCurrentFolder[i];
-      if (filepath.indexOf('index.njk') != -1) continue;
-      let parsedPath = _path2.default.parse(filepath);
-      let routeLink = fileRouteName + '/' + parsedPath.name;
-      let parentPath = req.path.replace('/' + parsedPath.name, '');
-
-      if (parentPath == fileRouteName) {
+      if (fileRouteName == req.path || fileRouteName + '/' == req.path || fileRouteName + '/index' == req.path) {
         parentActive = true;
       }
 
-      subItems.push({
-        name: (0, _lodash.startCase)(parsedPath.name),
-        link: routeLink,
-        viewFile: filepath,
-        active: routeLink == req.path,
-        parentPath
+      for (let i = 0; i < filePathsWithinCurrentFolder.length; i++) {
+        let filepath = filePathsWithinCurrentFolder[i];
+        if (filepath.indexOf('index.njk') != -1) continue;
+        let parsedPath = _path2.default.parse(filepath);
+        let routeLink = fileRouteName + '/' + parsedPath.name;
+        let parentPath = req.path.replace('/' + parsedPath.name, '');
+
+        if (parentPath == fileRouteName) {
+          parentActive = true;
+        }
+
+        subItems.push({
+          name: (0, _lodash.startCase)(parsedPath.name),
+          link: routeLink,
+          viewFile: filepath,
+          active: routeLink == req.path,
+          parentPath
+        });
+      }
+
+      navigationItems.push({
+        name: (0, _lodash.startCase)(foldername),
+        link: fileRouteName,
+        active: parentActive,
+        viewFile: _path2.default.join(currentPath, 'index.njk'),
+        subItems
       });
     }
 
-    navigationItems.push({
-      name: (0, _lodash.startCase)(foldername),
-      link: fileRouteName,
-      active: parentActive,
-      viewFile: _path2.default.join(currentPath, 'index.njk'),
-      subItems
-    });
-  }
+    req.libraryNavgiationItems = navigationItems;
 
-  req.libraryNavgiationItems = navigationItems;
+    next();
+  });
 
-  next();
-};
+  return function addLibraryNavigationItemsToRequestObject(_x2, _x3, _x4) {
+    return _ref2.apply(this, arguments);
+  };
+})();

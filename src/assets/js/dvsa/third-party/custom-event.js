353c264a892250b9aa53522020316024238dfb52
Tameem Safi <tameem@safi.me.uk>
2018-02-02 13:33:10 +0000

    Prettier

--- src/assets/js/dvsa/third-party/custom-event.js
+++ src/assets/js/dvsa/third-party/custom-event.js
@@ -1,17 +1,15 @@
 // More info: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
-(function () {
+(function() {
+  if (typeof window.CustomEvent === 'function') return false;
 
-  if ( typeof window.CustomEvent === "function" ) return false;
-
-  function CustomEvent ( event, params ) {
+  function CustomEvent(event, params) {
     params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
     return evt;
   }
 
   CustomEvent.prototype = window.Event.prototype;
 
   window.CustomEvent = CustomEvent;
-
 })();

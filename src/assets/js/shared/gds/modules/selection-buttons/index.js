84c73e74efb863f7063de4f14c96e34ab886d75e
Tameem Safi <tameem@safi.me.uk>
2018-02-06 16:13:37 +0000

    Updated MTS block labels scss

--- src/assets/js/shared/gds/modules/selection-buttons/index.js
+++ src/assets/js/shared/gds/modules/selection-buttons/index.js
@@ -18,7 +18,7 @@ export class SelectionButtons {
     this.ariaAttributes = {
       controls: 'aria-controls',
       expanded: 'aria-expanded',
-      hidden: 'aria-hidden'
+      hidden: 'aria-hidden',
     };
 
     this.init();
@@ -26,7 +26,7 @@ export class SelectionButtons {
 
   /**
    * Initializer function
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
@@ -37,7 +37,7 @@ export class SelectionButtons {
 
   /**
    * Attach DOM event handlers
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
@@ -50,17 +50,17 @@ export class SelectionButtons {
 
   /**
    * Radio click handler
-   * 
+   *
    * @param {Event} event Event object
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  radioClickHandler = (event) => {
-    if(!event.target) return;
+  radioClickHandler = event => {
+    if (!event.target) return;
     const element = event.target;
     const elementDetails = this.getElementDetails(element);
-    if(!elementDetails) return;
+    if (!elementDetails) return;
 
     // Reset all other elements
     groupElements.forEach(groupElement => {
@@ -70,60 +70,60 @@ export class SelectionButtons {
 
     // Update current element
     return this.updateDomElementAttributes(element);
-  }
+  };
 
   /**
    * Checkbox click handler
-   * 
+   *
    * @param {Event} event Event object
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  checkboxClickHandler = (event) => {
-    if(!event.target) return;
+  checkboxClickHandler = event => {
+    if (!event.target) return;
     const element = event.target;
     const elementDetails = this.getElementDetails(element);
-    if(!elementDetails) return;
+    if (!elementDetails) return;
     // Update current element
     return this.updateDomElementAttributes(element);
-  }
+  };
 
   /**
    * Focus in handler
-   * 
+   *
    * @param {Event} event Event object
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  focusInHandler = (event) => {
-    if(!event.target) return;
+  focusInHandler = event => {
+    if (!event.target) return;
     const element = event.target;
     const elementDetails = this.getElementDetails(element);
-    if(!elementDetails) return;
-    toggleClass(this.elementDetails.target, this.classnames.focused, true);
-  }
+    if (!elementDetails) return;
+    toggleClass(elementDetails.target, this.classnames.focused, true);
+  };
 
   /**
    * Focus out handler
-   * 
+   *
    * @param {Event} event Event object
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  focusOutHandler = (event) => {
-    if(!event.target) return;
+  focusOutHandler = event => {
+    if (!event.target) return;
     const element = event.target;
     const elementDetails = this.getElementDetails(element);
-    if(!elementDetails) return;
-    toggleClass(this.elementDetails.target, this.classnames.focused, false);
-  }
+    if (!elementDetails) return;
+    toggleClass(elementDetails.target, this.classnames.focused, false);
+  };
 
   /**
    * Update all aria attributes
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
@@ -132,57 +132,61 @@ export class SelectionButtons {
       // Update aria attributes
       this.updateAriaAttributesForElement(element);
     });
-  }
+  };
 
   /**
    * Update the current element attributes based on DOM state
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  updateDomElementAttributes = (element) => {
+  updateDomElementAttributes = element => {
     const elementDetails = this.getElementDetails(element);
-    if(!elementDetails) return;
+    if (!elementDetails) return;
     // Update selected class
     toggleClass(elementDetails.label, this.classnames.selected, element.checked);
+
     // Hide/show target
-    groupElementDetails.target.display = element.checked ? 'block' : 'none';
+    if (elementDetails.target) {
+      elementDetails.target.display = element.checked ? 'block' : 'none';
+    }
+
     // Update aria attributes
     this.updateAriaAttributesForElement(element);
-  }
+  };
 
   /**
    * Reset all of the aria attributes for all checkbox/radios
-   * 
+   *
    * @param {HTMLElement} element Element to change the aria attributes of
    * @param {String} targetId ID selector for the target of the current radio
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  updateAriaAttributesForElement = (element) => {
-    if(!element) return;
+  updateAriaAttributesForElement = element => {
+    if (!element) return;
     const elementDetails = this.getElementDetails(element);
-    if(elementDetails) {
+    if (elementDetails) {
       element.setAttribute(this.ariaAttributes.controls, elementDetails.targetId);
     }
     element.setAttribute(this.ariaAttributes.hidden, element.checked ? 'false' : 'true');
     element.setAttribute(this.ariaAttributes.expanded, element.checked ? 'true' : 'false');
-  }
+  };
 
   /**
    * Get the target of the element
-   * 
+   *
    * @since 1.1.0
    * @author Tameem Safi <t.safi@kainos.com>
    */
-  getElementDetails = (element) => {
-    if(!element) return;
+  getElementDetails = element => {
+    if (!element) return;
     const groupName = element.getAttribute('name');
     const groupElements = Array.from(document.querySelectorAll(`.${this.classnames.blockLabel} input[name="${groupName}"]`));
     const label = closestParentOfEl(element, 'label');
 
-    if(!label) {
+    if (!label) {
       return console.warn('Radio parent label not found');
     }
 
@@ -194,8 +198,7 @@ export class SelectionButtons {
       groupElements,
       label,
       target,
-      targetId
+      targetId,
     };
-  }
-
+  };
 }

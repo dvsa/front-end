c2a6f00ee2de4fa6ad31926b8a0403230f3ff8d2
James Nelson <Jamesnelson117@gmail.com>
2018-03-02 11:47:38 +0000

    JN - Functionality of admin assessment tool complete

--- src/server/controllers/annual-assessment-tool/suspend-users.js
+++ src/server/controllers/annual-assessment-tool/suspend-users.js
@@ -1,24 +1,22 @@
 import * as uploadHelpers from './helpers/upload-helpers';
 
 export const getSuspendUsers = (req, res) => {
-  const { viewData } = req.session.errors;
-  return res.redirect('/prototypes/annual-assessment-admin-tool/suspend-users/index', viewData);    
+  return res.render('prototypes/annual-assessment-admin-tool/suspend-users/index', { errors: req.session.errors });
 };
 
 export const postSuspendUsers = (req, res) => {
-  if (!req.body['file-upload']) {
-    errors.push(["Please ensure you've uploaded a file"]);
-    req.session.errors.push(["Please ensure you've uploaded a file"]);
-  } 
-
+  req.session.errors = [];
+  // If file was not uploaded then add an error message to session
+  if (!req.body['file-upload']) req.session.errors.push('Please upload a file');
+  // Assigns boolean value from file extension check
   const isCSV = uploadHelpers.isFileExtensionOfType(uploadHelpers.getFileExtension(req.body['file-upload']), 'CSV');
-  
-  if (req.body['file-upload'] && !isCSV) req.session.errors.push(['File type must be of CSV']);
-  
-  if (!req.session.errors) {
-    return res.redirect('/prototypes/annual-assessment-admin-tool/suspend-users/review');   
+  // If file extension is not CSV then add an error message to session
+  if (req.body['file-upload'] && !isCSV) req.session.errors.push('File type must be of CSV');
+  // If no errors exist in session
+  if (!req.session.errors.length) {
+    // Clear session
+    req.session.errors = null;
+    return res.redirect('/prototypes/annual-assessment-admin-tool/suspend-users/review');
   }
-
-  return res.redirect('/prototypes/annual-assessment-admin-tool/suspend-users/index');
+  return res.redirect('/prototypes/annual-assessment-admin-tool/suspend-users/');
 };
-

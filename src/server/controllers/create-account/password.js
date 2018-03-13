fdc6449775b38e3b9293f41fbdd2946ad04861c7
Tameem Safi <tameem@safi.me.uk>
2018-03-13 14:55:09 +0000

    Fixed create account user journey validation

--- src/server/controllers/create-account/password.js
+++ src/server/controllers/create-account/password.js
@@ -1,6 +1,30 @@
-import { renderViewWithValuesOrRedirect, renderWithErrorsOrRedirectWithSession } from './helpers';
+import { renderViewWithValuesOrRedirect, renderWithErrorsOrRedirectWithSession, addFormValuesToSession } from './helpers';
 
 export const getPassword = (req, res) => {
+  // Only testing password route
+  if (req.query.testPasswordJouneyOnly) {
+    // Reset session with pre-filled data
+    req.session.createAccountForm = {};
+    req.session = addFormValuesToSession(req.session, {
+      errors: false,
+      email: 'test@google.com',
+      firstname: 'Test',
+      lastname: 'Test',
+      day: 1,
+      month: 1,
+      year: 1980,
+      address1: '1 test road',
+      townOrCity: 'London',
+      postCode: 'SW1A 1AA',
+      phoneNumber: '12345678910',
+      questionOne: 1,
+      questionOneAnswer: 'answer1',
+      questionTwo: 2,
+      questionTwoAnswer: 'answer2',
+    });
+    // Redirect back to password route
+    return res.redirect('/prototypes/create-account/password');
+  }
   return renderViewWithValuesOrRedirect(req, res, 'prototypes/create-account/password');
 };
 
--- src/server/controllers/create-account/validators/password.js
+++ src/server/controllers/create-account/validators/password.js
@@ -4,43 +4,46 @@ const weakPasswordMessage = 'Your password cannot contain; username, personal in
 
 export const passwordValidationChecks = [
   check('password')
-    .exists()
     .trim()
-    .not()
+    .exists()
     .withMessage('Password cannot be empty')
     .isLength({ min: 8 })
     .withMessage('Password must be atleast 8 characters')
-    // Blocked word check
-    .contains('password')
-    .withMessage(weakPasswordMessage)
-    // Blocked word check
-    .contains('blocked_word')
+    // Blocked words check
+    .custom((value, { req }) => {
+      const blockedWords = ['password', 'animal', 'cat', 'dog'];
+      return !blockedWords.contains(value);
+    })
     .withMessage(weakPasswordMessage)
     // Personal details check
-    .custom((value, { req }) => value === req.body.email)
-    .isEmail()
-    .withMessage(weakPasswordMessage)
-    .custom((value, { req }) => value === req.body.firstname)
-    .withMessage(weakPasswordMessage)
-    .custom((value, { req }) => value === req.body.middlename)
-    .withMessage(weakPasswordMessage)
-    .custom((value, { req }) => value === req.body.lastname)
+    .custom((value, { req }) => {
+      const items = ['email', 'firstname', 'middlename', 'lastname'];
+      let isValid = true;
+      items.forEach(item => {
+        if (req.body[item]) {
+          const itemLowercase = req.body[item].toLowerCase();
+          isValid = !itemLowercase.contains(value.toLowerCase());
+        }
+      });
+      return isValid;
+    })
     .withMessage(weakPasswordMessage)
     // Contains mixed characters
     .custom((value, { req }) => {
-      !/^(?=.*[a-z])(?=.*[A-Z]).+$/.test(value);
+      return /^(?=.*[a-z])(?=.*[A-Z]).+$/.test(value);
     })
     .withMessage('Password must contain both uppercase and lowercase characers')
     // Has numeric value
-    .custom((value, { req }) => !/[0-9]/.test(value))
-    .withMessage(weakPasswordMessage)
+    .custom((value, { req }) => {
+      return /[0-9]/.test(value);
+    })
+    .withMessage('Password must contain atleast 1 number')
     // Check it matches confirmation
     .custom((value, { req }) => value === req.body.passwordConfirmation)
     .withMessage('Password does not match confirmation'),
 
   check('passwordConfirmation')
     .exists()
-    .not()
     .withMessage('Password confirmation cannot be empty')
     .custom((value, { req }) => value === req.body.password)
     .withMessage('Confirmation does not match original password'),

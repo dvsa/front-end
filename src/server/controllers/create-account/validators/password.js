d34d0bbc53b9cffb0b2a68362bf8cbafc5cd1848
Tameem Safi <tameem@safi.me.uk>
2018-03-13 11:25:04 +0000

    Added extra password validations for create account journey

--- src/server/controllers/create-account/validators/password.js
+++ src/server/controllers/create-account/validators/password.js
@@ -1,20 +1,46 @@
 import { check } from 'express-validator/check';
 
+const weakPasswordMessage = 'Your password cannot contain; username, personal information or any blocked words';
+
 export const passwordValidationChecks = [
   check('password')
     .exists()
     .trim()
     .not()
-    .isEmail()
     .withMessage('Password cannot be empty')
     .isLength({ min: 8 })
     .withMessage('Password must be atleast 8 characters')
+    // Blocked word check
+    .contains('password')
+    .withMessage(weakPasswordMessage)
+    // Blocked word check
+    .contains('blocked_word')
+    .withMessage(weakPasswordMessage)
+    // Personal details check
+    .custom((value, { req }) => value === req.body.email)
+    .isEmail()
+    .withMessage(weakPasswordMessage)
+    .custom((value, { req }) => value === req.body.firstname)
+    .withMessage(weakPasswordMessage)
+    .custom((value, { req }) => value === req.body.middlename)
+    .withMessage(weakPasswordMessage)
+    .custom((value, { req }) => value === req.body.lastname)
+    .withMessage(weakPasswordMessage)
+    // Contains mixed characters
+    .custom((value, { req }) => {
+      !/^(?=.*[a-z])(?=.*[A-Z]).+$/.test(value);
+    })
+    .withMessage('Password must contain both uppercase and lowercase characers')
+    // Has numeric value
+    .custom((value, { req }) => !/[0-9]/.test(value))
+    .withMessage(weakPasswordMessage)
+    // Check it matches confirmation
     .custom((value, { req }) => value === req.body.passwordConfirmation)
     .withMessage('Password does not match confirmation'),
+
   check('passwordConfirmation')
     .exists()
     .not()
-    .isEmpty()
     .withMessage('Password confirmation cannot be empty')
     .custom((value, { req }) => value === req.body.password)
     .withMessage('Confirmation does not match original password'),

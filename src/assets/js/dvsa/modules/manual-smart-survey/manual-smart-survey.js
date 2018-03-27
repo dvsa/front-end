d2106676a5884629abfb7faba44be71f38b087b9
Tameem Safi <tameem@safi.me.uk>
2018-03-27 16:57:02 +0100

    Build production v1.1.25

--- src/assets/js/dvsa/modules/manual-smart-survey/manual-smart-survey.js
+++ src/assets/js/dvsa/modules/manual-smart-survey/manual-smart-survey.js
@@ -5,17 +5,17 @@ export class ManualSmartSurvey {
     this.classnames = {
       manualSmartSurvey: {
         base: 'manual-smart-survey',
-        iframe: 'manual-smart-survey__iframe'
-      }
+        iframe: 'manual-smart-survey__iframe',
+      },
     };
 
     this.attributes = {
       iframeSrc: 'data-iframe-src',
-      iframeAttached: 'data-iframe-attached'
+      iframeAttached: 'data-iframe-attached',
     };
 
     this.elements = {
-      smartSurveyElements: Array.from(document.querySelectorAll(`.${this.classnames.manualSmartSurvey.base}`))
+      smartSurveyElements: Array.from(document.querySelectorAll(`.${this.classnames.manualSmartSurvey.base}`)),
     };
 
     this.init();
@@ -25,45 +25,45 @@ export class ManualSmartSurvey {
    * Initializer
    * - Add iframes for visible elements
    * - Attach events
-   * 
+   *
    * @author Tameem Safi <t.safi@kainos.com>
    * @since 1.1.25
    */
   init = () => {
     this.setupAllIframes();
     addEventListenerToEl(window, 'load resize scroll', this.onWindowLoadResizeScroll);
-  }
+  };
 
   /**
    * Add iframe to all visible elements
-   * 
+   *
    * @author Tameem Safi <t.safi@kainos.com>
    * @since 1.1.25
    */
   setupAllIframes = () => {
     this.elements.smartSurveyElements.forEach(element => {
       // Check to see if element is in viewport
-      if(!isElementInViewport(element)) return;
-      
+      if (!isElementInViewport(element)) return;
+
       // Check if iframe has already been added
       const iframeAttached = element.getAttribute(this.attributes.iframeAttached) || false;
-      if(iframeAttached) return;
+      if (iframeAttached) return;
 
       // Add iframe
       const iframeSrc = element.getAttribute(this.attributes.iframeSrc);
       element.innerHTML = this.generateIframeCode(iframeSrc);
     });
-  }
+  };
 
   /**
    * Generates the html for the iframe
-   * 
+   *
    * @param {String} src Src of the iframe
-   * 
+   *
    * @author Tameem Safi <t.safi@kainos.com>
    * @since 1.1.25
    */
-  generateIframeCode = (src) => {
+  generateIframeCode = src => {
     return `
       <iframe
         src="${src}"
@@ -71,5 +71,5 @@ export class ManualSmartSurvey {
         class="${this.classnames.manualSmartSurvey.iframe}"
       ></iframe>
     `;
-  }
+  };
 }
--- src/assets/js/shared/misc/is-element-in-viewport.js
+++ src/assets/js/shared/misc/is-element-in-viewport.js
@@ -3,7 +3,7 @@
  *
  * Tested with:
  * IE7+, iOS5+ Safari, Android2+, Blackberry, Opera Mobile, and IE Mobile 10.
- * 
+ *
  * See:
  * https://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
  *
@@ -12,7 +12,7 @@
  * @author Tameem Safi <t.safi@kainos.com>
  * @since 1.1.25
  */
-export function isElementInViewport (el) {
+export function isElementInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
     rect.top >= 0 &&

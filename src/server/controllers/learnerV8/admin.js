208b4cfe5c0339ff68610d9b91a8e28f4ab4896c
Joel Sudworth <j.sudworth2@kainos.com>
2018-05-23 12:17:22 +0100

    changing the link again

--- src/server/controllers/learnerV8/admin.js
+++ src/server/controllers/learnerV8/admin.js
@@ -15,20 +15,6 @@ export function adminPost(req, res) {
   return res.redirect('/prototypes/learner/v8/admin');
 }
 
-// admin add
-export function adminAddGet(req, res) {
-  let viewData;
-
-  viewData = {};
-
-  return res.render('prototypes/learner/v8/admin/add', viewData);
-}
-export function adminAddPost(req, res) {
-  const {} = req.body;
-
-  return res.redirect('/prototypes/learner/v8/admin');
-}
-
 // admin edit
 export function adminEditGet(req, res) {
   let viewData;
@@ -41,4 +27,34 @@ export function adminEditPost(req, res) {
   const {} = req.body;
 
   return res.redirect('/prototypes/learner/v8/admin');
+}
+
+
+// admin add
+export function adminAddGet(req, res) {
+    let viewData;
+
+    viewData = {};
+
+    return res.render('prototypes/learner/v8/admin/add', viewData);
+}
+export function adminAddPost(req, res) {
+    const {} = req.body;
+
+    return res.redirect('/prototypes/learner/v8/admin-details');
+}
+
+
+// admin add 2
+export function adminAddDetailsGet(req, res) {
+    let viewData;
+
+    viewData = {};
+
+    return res.render('prototypes/learner/v8/admin/add-details', viewData);
+}
+export function adminAddDetailsPost(req, res) {
+    const {} = req.body;
+
+    return res.redirect('/prototypes/learner/v8/admin');
 }

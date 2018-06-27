5fc34085805691ec82abab5f8779dac53b1b28a1
Joel Sudworth <j.sudworth2@kainos.com>
2018-06-27 10:53:37 +0100

    moving to branches

---
+++ src/server/controllers/learnerV10/booking.js
@@ -0,0 +1,67 @@
+// Booking
+// get course data
+let generalData = require('./data');
+
+export function bookingDateGet(req, res) {
+  let viewData, courseDates;
+
+  courseDates = generalData.courseDates;
+
+  viewData = {
+    courseDates,
+  };
+
+  return res.render('prototypes/learner/v10/booking/index', viewData);
+}
+
+export function bookingDatePost(req, res) {
+  const { availableCourses } = req.body;
+
+  // availableCourses
+
+  return res.redirect('/prototypes/learner/v10/booking/payment');
+}
+
+// PAYMENT
+
+export function bookingPaymentGet(req, res) {
+  let viewData;
+
+  viewData = {};
+
+  return res.render('prototypes/learner/v10/booking/payment', viewData);
+}
+
+export function bookingPaymentPost(req, res) {
+  return res.redirect('/prototypes/learner/v10/booking/confirm');
+}
+
+// CONFIRM
+
+export function bookingConfirmGet(req, res) {
+  let viewData;
+
+  viewData = {};
+
+  return res.render('prototypes/learner/v10/booking/confirm', viewData);
+}
+
+// COMPLETE
+
+export function bookingCompleteGet(req, res) {
+  let viewData;
+
+  viewData = {};
+
+  return res.render('prototypes/learner/v10/booking/complete', viewData);
+}
+
+// BOOKING ELEMENTS
+
+export function bookingPOCGet(req, res) {
+  let viewData;
+
+  viewData = {};
+
+  return res.render('prototypes/layouts/booking/index', viewData);
+}

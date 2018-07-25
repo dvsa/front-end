9fe3ceff1a40015678b702a9edac4261fb7ad676
Martin Di Martino -Marriott <hello@martindm.uk>
2018-07-25 23:41:38 +0100

    Route progress

---
+++ src/server/controllers/site-review/initialData.js
@@ -0,0 +1,14 @@
+export const initialData = () => {
+  return {
+    testerName: 'James',
+    'self-completed': true,
+    'ae-representatives-user-id': 'Testers ID',
+    'ae-representatives-full-name': 'Martin',
+    'testers-user-id': 'Tester',
+    date: {
+      day: '14',
+      month: '05',
+      year: '2018',
+    },
+  };
+};
--- src/server/controllers/site-review/site-review.js
+++ src/server/controllers/site-review/site-review.js
@@ -1,65 +1,51 @@
 //import { renderViewWithValuesOrRedirect, renderWithErrorsOrRedirectWithSession } from './helpers';
 import { addToSession } from '../speech-to-text-search/helpers/add-to-session.js';
+import { initialData } from './initialData';
 
-// Get view data into template
-const initialData = () => {
-  return (startData = {
-    testerName: 'James',
-    'self-completed': true,
-    'ae-representatives-user-id': 'Testers ID',
-    'ae-representatives-full-name': 'Martin',
-    'testers-user-id': 'Tester',
-    date: {
-      day: '14',
-      month: '05',
-      year: '2018',
-    },
-  });
+// not used yet
+export const getDetails = (req, res) => { 
+  req.session.viewData = initialData(); 
+  const viewData = req.session;
+  console.log('viewdata', viewData);
+  res.render('./prototypes/site-review/enter-details/index', { viewData: viewData || {} });
 };
 
-export const getDetails = (req, res) => {
-    req.session.name = "martin";
-    const viewData = req.session;
-    //console.log(viewData);
-    res.render('./prototypes/site-review/enter-details/index', { viewData: viewData || {} });
-}
+export const postDetails = (req, res) => { 
 
-export const postDetails = (req, res) => {
-
-  // add more info to session
+// add more info to session
   req.body.outcomes = {
-      "compliance": {
-          "outcome" : "Improve",
-          "advice" : "Compliance comment"
-      },
-      "premises": {
-        "outcome" : "Satisfactory",
-        "advice" : "Premises comment"
-      },
-      "management": {
+    "compliance": {
         "outcome" : "Improve",
-        "advice" : "Management and people comment"
-      },
-      "people": {
-        "outcome" : "Unsatisfactory",
-        "advice" : "Improvements needed..."
-      },
-      "activity": {
-        "outcome" : "Activity performed",
-        "detail" : "1234567987"
-      }
-  };
+        "advice" : "Compliance comment"
+    },
+    "premises": {
+      "outcome" : "Satisfactory",
+      "advice" : "Premises comment"
+    },
+    "management": {
+      "outcome" : "Improve",
+      "advice" : "Management and people comment"
+    },
+    "people": {
+      "outcome" : "Unsatisfactory",
+      "advice" : "Improvements needed..."
+    },
+    "activity": {
+      "outcome" : "Activity performed",
+      "detail" : "1234567987"
+    }
+}; 
+  // Add initial data to req body
+  req.body.viewData = initialData();
 
-  // Add answers to viewdata
-  req.session.viewData = req.body; 
-  console.log('stage 1: ', req.session.viewData)
+  // Now set viewdata to be all our form data from the whole journey
+  req.session.viewData = req.body;
   return res.redirect('/prototypes/site-review/summary/');
 };
 
-
 export const getSummary = (req, res) => {
-    //req.session.viewData = req.body;
-    const viewData = req.session.viewData;
-    console.log('stage 2', viewData);
-    res.render('./prototypes/site-review/summary/index', { viewData: viewData });
+  //req.session.viewData = req.body;
+  const viewData = req.session.viewData;
+  console.log(viewData);
+  res.render('./prototypes/site-review/summary/index', { viewData: viewData });
 };

60b4d1215c3349b11cef1d64cd079848d93843a1
Martin Di Martino -Marriott <hello@martindm.uk>
2018-07-26 12:32:08 +0100

    Populating Summary page

---
+++ src/server/controllers/site-review/initViewData.js
@@ -0,0 +1,103 @@
+/**
+ * Sets and returns an empty site review view data object
+ *
+ * @returns {Object} - Site review view data object
+ */
+export const initViewData = () => {
+  return {
+    testerDetails: {
+      date: '26 July 2018',
+      testDay: '10',
+      testMonth: 'July',
+      testYear: '2018'
+    },
+    activity: {
+      activityPerformed: true,
+      motTestNum: '1234567890',
+      reasonCommited: 'Test not necessary',
+      errors: [],
+    },
+    compliance: {
+      commitedLevel: 'Satisfactory',
+      commitedComment: 'Information given in writing to site manager',
+      errors: [],
+      satisfactory: {
+        isChecked: true,
+        comment: '',
+      },
+      improve: {
+        isChecked: false,
+
+        comment: '',
+      },
+      unsatisfactory: {
+        isChecked: false,
+        comment: '',
+      },
+    },
+    'management-and-quality': {
+      commitedLevel: 'Unsatisfactory',
+      commitedComment: 'Corrective advice given to testing staff',
+      errors: [],
+      satisfactory: {
+        isChecked: true,
+        comment: '',
+      },
+      improve: {
+        isChecked: false,
+        comment: '',
+      },
+      unsatisfactory: {
+        isChecked: false,
+        comment: '',
+      },
+    },
+    people: {
+      commitedLevel: 'Satisfactory',
+      commitedComment: 'Information given in writing to site manager',
+      errors: [],
+      satisfactory: {
+        isChecked: true,
+        comment: '',
+      },
+      improve: {
+        isChecked: false,
+        comment: '',
+      },
+      unsatisfactory: {
+        isChecked: false,
+        comment: '',
+      },
+    },
+    premises: {
+      commitedLevel: 'Satisfactory',
+      commitedComment: 'Information given in writing to site manager',
+      errors: [],
+      satisfactory: {
+        isChecked: true,
+        comment: '',
+      },
+      improve: {
+        isChecked: false,
+        comment: 'Improvements advised for premises',
+      },
+      unsatisfactory: {
+        isChecked: false,
+        comment: 'Urgent improvements advised for premises',
+      },
+    },
+  };
+}
+    /* testerName: 'James',
+    'self-completed': true,
+    'ae-representatives-user-id': 'Testers ID',
+    'ae-representatives-full-name': 'Martin',
+    'testers-user-id': 'Tester',
+    date: {
+      day: '14',
+      month: '05',
+      year: '2018',
+    }, */
+/*   };
+};
+ */
--- src/server/controllers/site-review/site-review.js
+++ src/server/controllers/site-review/site-review.js
@@ -1,51 +1,45 @@
 //import { renderViewWithValuesOrRedirect, renderWithErrorsOrRedirectWithSession } from './helpers';
 import { addToSession } from '../speech-to-text-search/helpers/add-to-session.js';
-import { initialData } from './initialData';
+import { initViewData } from './initViewData.js';
+import { getMonth } from './helpers/helpers';
 
-// not used yet
-export const getDetails = (req, res) => { 
-  req.session.viewData = initialData(); 
+// Not used yet
+export const getDetails = (req, res) => {
+  req.session.viewData = initialData();
   const viewData = req.session;
   console.log('viewdata', viewData);
   res.render('./prototypes/site-review/enter-details/index', { viewData: viewData || {} });
 };
 
+
 export const postDetails = (req, res) => { 
-
-// add more info to session
-  req.body.outcomes = {
-    "compliance": {
-        "outcome" : "Improve",
-        "advice" : "Compliance comment"
-    },
-    "premises": {
-      "outcome" : "Satisfactory",
-      "advice" : "Premises comment"
-    },
-    "management": {
-      "outcome" : "Improve",
-      "advice" : "Management and people comment"
-    },
-    "people": {
-      "outcome" : "Unsatisfactory",
-      "advice" : "Improvements needed..."
-    },
-    "activity": {
-      "outcome" : "Activity performed",
-      "detail" : "1234567987"
-    }
-}; 
+  
   // Add initial data to req body
-  req.body.viewData = initialData();
+  const viewData = initViewData();
+  const testerDetails = req.body; 
+  
+  //testerDetails.date = date;
+  const dateString = `${testerDetails.testDay} ${getMonth( testerDetails.testMonth-1)} ${testerDetails.testYear}`;
+ 
+  // Check we have a valid date string
+  if ( dateString.indexOf('undefined') >= 0 ) {
+      testerDetails.date = testerDetails.date;
+  } else {
+    testerDetails.date = dateString;
+  } 
 
-  // Now set viewdata to be all our form data from the whole journey
-  req.session.viewData = req.body;
+  // Apply all form info to Viewdata in session
+  viewData.testerDetails = testerDetails;
+  req.session.viewData = viewData;
+  
+  // Now set template data to be all our form data from the whole journey
+  //viewData = req.session.viewData;
+  console.log(req.session.viewData);
   return res.redirect('/prototypes/site-review/summary/');
 };
 
 export const getSummary = (req, res) => {
   //req.session.viewData = req.body;
   const viewData = req.session.viewData;
-  console.log(viewData);
   res.render('./prototypes/site-review/summary/index', { viewData: viewData });
 };

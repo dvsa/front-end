0e36660717bbe292dda68e631366a449810e33d2
Martin Di Martino -Marriott <hello@martindm.uk>
2018-08-03 14:31:07 +0100

    Brake test route and layout

--- src/server/controllers/brake-test/brake-test.js
+++ src/server/controllers/brake-test/brake-test.js
@@ -6,5 +6,16 @@
  */
 export const postBrakeConfig = (req, res) => {
   // Redirect to enter brake test details
-  return res.redirect('/prototypes/brake-test-entry/');
+  return res.redirect('/prototypes/brake-test-entry');
+};
+
+/**
+ * POST request middleware - On post to brake test entries, direct to summary
+ *
+ * @param {Express.Request} req - Express request object
+ * @param {Express.Response} res - Express response object
+ */
+export const postBrakeEntry = (req, res) => {
+  // Redirect to enter brake test details
+  return res.redirect('/prototypes/brake-test-summary');
 };

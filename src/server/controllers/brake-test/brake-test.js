08f2b7f4a69be9776f4b63f7ab9c7b905f88454f
Martin Di Martino -Marriott <hello@martindm.uk>
2018-08-03 12:50:27 +0100

    Linted

--- src/server/controllers/brake-test/brake-test.js
+++ src/server/controllers/brake-test/brake-test.js
@@ -1,12 +1,10 @@
-
 /**
  * POST request middleware - On post to brake test config, direct to enter details
  *
  * @param {Express.Request} req - Express request object
  * @param {Express.Response} res - Express response object
  */
-export const postBrakeConfig = (req, res) => { 
-    // Redirect to enter brake test details
-    return res.redirect('/prototypes/brake-test-entry/');
+export const postBrakeConfig = (req, res) => {
+  // Redirect to enter brake test details
+  return res.redirect('/prototypes/brake-test-entry/');
 };
-  

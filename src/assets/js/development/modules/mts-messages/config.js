624969ef148918c39c4c3cdf402589ef553bc4a9
Martin Di Martino -Marriott <hello@martindm.uk>
2018-08-14 22:09:53 +0100

    Rebasing

--- src/assets/js/development/modules/mts-messages/config.js
+++ src/assets/js/development/modules/mts-messages/config.js
@@ -1,5 +1,6 @@
 export const FILTER_CONFIG = {
 <<<<<<< HEAD
+<<<<<<< HEAD
   selectors: {
     component: '.js-filter-wrapper',
     messageList: '.js-message-list',
@@ -20,5 +21,17 @@ export const FILTER_CONFIG = {
   dataAttributes: {
     type: 'data-message', // 'special', 'advice', 'system'
 >>>>>>> Filtering WIP
+=======
+  selectors: {
+    component: '.js-filter-wrapper',
+    messageList: '.js-filtered-messages',
+    checkboxes: '.js-message-filter', 
+    filteredView: '.js-message-panel',
+    emptyNotice: 'message-panel__notice' // used inside a method, so omit the dot
+>>>>>>> Filter functionality
   },
+  data: {
+    emptyNotice: 'No messages to show',
+    messages: '[data-type]'
+  }
 };

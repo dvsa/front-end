4b18d2b33435b375de0639226d31c170966d9c5a
Martin Di Martino -Marriott <hello@martindm.uk>
2018-08-16 13:41:51 +0100

    rebasing

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -105,7 +105,7 @@ router.post('/prototypes/brake-test-entry', brakeTestController.postBrakeEntry);
 // MTS Messaging
 router.param('messageIndex', messagingController.isValidMessage);
 router.get('/prototypes/messaging', [messagingController.setupMessages, messagingController.getMessages]);
-router.get('/prototypes/messaging/archive', [messagingController.setupMessages, messagingController.getArchive]);
+router.get('/prototypes/messaging/archive', [messagingController.getArchive]);
 router.get('/prototypes/messaging/:messageIndex', messagingController.getMessage);
 router.get('/prototypes/messaging/acknowledge/:messageIndex', [
   messagingController.unpinSpecialNotice,

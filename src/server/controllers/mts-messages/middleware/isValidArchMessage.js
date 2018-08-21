2b209fe467ff637369fbcae00b7c9fa5d5d78060
Martin Di Martino -Marriott <hello@martindm.uk>
2018-08-21 13:43:51 +0100

    Archive routes

--- src/server/controllers/mts-messages/middleware/isValidArchMessage.js
+++ src/server/controllers/mts-messages/middleware/isValidArchMessage.js
@@ -1,5 +1,5 @@
 /**
- * GET Middleware - Checks to see if message exists in archive array
+ * GET Middleware - Checks to see if message exists in messages array
  *
  * @param {Express.Request} req - Express request object
  * @param {Express.Response} res - Express response object
@@ -7,15 +7,14 @@
  * @param {Int} messageID - Message ID number
  */
 export const isValidArchMessage = (req, res, next, messageID) => {
-  // If messages on req.session.viewData non existent or message
-  // does not exist in archive
-  if (!req.session.viewData.archive || !req.session.viewData.archive[messageID]) {
+  // If messages in req.session.viewData non existent or message
+  // does not exist in messages array
+  if (!req.session.viewData.messages || !req.session.viewData.messages[messageID]) {
     // redirect back to messaging view
     return res.redirect('/prototypes/messaging/');
   }
-
-  // Attached message to req.message
-  req.message = req.session.viewData.archive[messageID];
+  // Attaches message to req.message for next middleware
+  req.message = req.session.viewData.messages[messageID];
 
   // Run next middleware stack
   next();

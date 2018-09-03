91a3e74e697264fb03bbb3557ee382998bcd6784
Martin Di Martino -Marriott <hello@martindm.uk>
2018-09-03 12:19:12 +0100

    Built prod for Heroku

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -4,7 +4,7 @@
  *   Version: 1.2.22
  *   Author: Tameem Safi <https://github.com/tameemsafi>
  *   Contributors: Tameem Safi <https://github.com/tameemsafi>, James Nelson <j.nelson@kainos.com>
- *   Timestamp: August 31st 2018, 3:38:24 pm
+ *   Timestamp: September 3rd 2018, 12:18:41 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

77a1b4af8a5623adfd035792656e0b98e173538d
James Nelson <Jamesnelson117@gmail.com>
2018-09-07 11:22:44 +0100

    Build production 1.2.23

--- dist/assets/javascripts/dvsa-mts-legacy.bundle.js
+++ dist/assets/javascripts/dvsa-mts-legacy.bundle.js
@@ -4,7 +4,7 @@
  *   Version: 1.2.22
  *   Author: Tameem Safi <https://github.com/tameemsafi>
  *   Contributors: Tameem Safi <https://github.com/tameemsafi>, James Nelson <j.nelson@kainos.com>
- *   Timestamp: September 7th 2018, 10:57:39 am
+ *   Timestamp: September 7th 2018, 11:21:19 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */

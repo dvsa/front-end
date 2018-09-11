5a7c9248c20f29d474a5cda61d7c6bcf93505ae2
Martin Di Martino -Marriott <hello@martindm.uk>
2018-09-11 16:18:50 +0100

    added styles

--- src/assets/js/development/modules/autocomplete/index.js
+++ src/assets/js/development/modules/autocomplete/index.js
@@ -1,6 +1,7 @@
 import accessibleAutocomplete from 'accessible-autocomplete';
 
 const countries = ['France', 'Germany', 'United Kingdom'];
+
 accessibleAutocomplete({
   element: document.querySelector('#autocomplete-container'),
   id: 'autocomplete', // To match it to the existing <label>.

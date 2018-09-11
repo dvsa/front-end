486eeb53ee2fdf7caf796a750e77f37681904820
Martin Di Martino -Marriott <hello@martindm.uk>
2018-09-11 15:42:12 +0100

    Build prod files

--- dist/assets/javascripts/dvsa-mts-legacy.bundle.js
+++ dist/assets/javascripts/dvsa-mts-legacy.bundle.js
@@ -4,7 +4,7 @@
  *   Version: 1.2.23
  *   Author: Tameem Safi <https://github.com/tameemsafi>
  *   Contributors: Tameem Safi <https://github.com/tameemsafi>, James Nelson <j.nelson@kainos.com>
- *   Timestamp: September 11th 2018, 12:17:29 pm
+ *   Timestamp: September 11th 2018, 3:41:24 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

fcc5941fc7715c481d25864c59f129bde5c8fd8c
James Nelson <Jamesnelson117@gmail.com>
2018-09-12 09:29:34 +0100

    Addded autocomplete functionality

--- src/assets/js/development/modules/autocomplete/index.js
+++ src/assets/js/development/modules/autocomplete/index.js
@@ -1,9 +1,22 @@
 import accessibleAutocomplete from 'accessible-autocomplete';
 
-const countries = ['France', 'Germany', 'United Kingdom'];
+const manufacturers = ['Bentley', 'BMW', 'Cadillac', 'Chevrolet', 
+'Citroen', 'Dacia', 'Dodge', 'Fiat', 'Ford', 'Honda', 'Hyundai', 'Jaguar',
+'Jeep', 'Kia', 'Land Rover', 'Mercedes Benz', 'Mini', 'Nissan', 'Peugeot', 
+'Renault', 'Skoda', 'Tesla', 'Toyota', 'Vauxhall', 'Volvo', 'VW'];
+
+const landRoverModels = ['All New Discovery', 'Defender', 'Discovery', 
+'Discovery Sport', 'Freelander', 'Land Rover', 'Range Rover', 'Range Rover Evoque',
+'Range Rover Sport', 'Range Rover Velar'];
 
 accessibleAutocomplete({
-  element: document.querySelector('#autocomplete-container'),
-  id: 'autocomplete', // To match it to the existing <label>.
-  source: countries,
+  element: document.querySelector('#autocomplete-manufacturers'),
+  id: 'autocomplete-manufacturers', // To match it to the existing <label>.
+  source: manufacturers
+});
+
+accessibleAutocomplete({
+  element: document.querySelector('#autocomplete-landRoverModels'),
+  id: 'autocomplete-landrovermodels', // To match it to the existing <label>.
+  source: landRoverModels,
 });

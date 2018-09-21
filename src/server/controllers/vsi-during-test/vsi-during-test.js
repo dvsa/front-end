c25168ef2e66d38e8b597bd12da876146e667849
Martin Di Martino -Marriott <hello@martindm.uk>
2018-09-21 12:40:33 +0100

    Odometer in session - WIP

--- src/server/controllers/vsi-during-test/vsi-during-test.js
+++ src/server/controllers/vsi-during-test/vsi-during-test.js
@@ -9,7 +9,8 @@ import { isEmpty } from '../vts-changes/helpers/helpers.js';
  */
 export const postOdometer = (req, res) => {
   req.session.viewData = req.body || initData();
-  return res.redirect('/prototypes/vsi-during-test/inspection');
+  console.log('posted odometer');
+  return res.redirect('/prototypes/vsi-during-test/inspection/inspection-4');
 };
 
 /**
@@ -18,8 +19,8 @@ export const postOdometer = (req, res) => {
  * @param {Express.Request} req - Express request object
  * @param {Express.Response} res - Express response object
  */
-export const getMotInspection = (req, res) => {
-  req.session.viewData = req.session.viewData || initData();
-  console.log(req.session.viewData);
-  return res.render('./prototypes/vsi-during-test/inspection/index', { viewData: req.session.viewData });
+export const getInspectionWithOdometer = (req, res) => {
+  req.session.viewData = req.body || initData();
+  console.log('get inspection 4');
+  return res.render('./prototypes/vsi-during-test/inspection/inspection-4', { viewData: req.session.viewData });
 };

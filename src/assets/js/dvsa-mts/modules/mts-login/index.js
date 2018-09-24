3a8e9cd0dec3df8725f44ac8e262259fa9a920dd
James Nelson <Jamesnelson117@gmail.com>
2018-09-24 15:39:38 +0100

    Updated functionality on mts-login.js (#130)

--- src/assets/js/dvsa-mts/modules/mts-login/index.js
+++ src/assets/js/dvsa-mts/modules/mts-login/index.js
@@ -1,10 +1,10 @@
 export const initMtsLogin = () => {
   const passwordField = document.querySelector('.sign-in-form .password-field');
-
+  
   if (!passwordField) return;
-
+  
   //Submit form when enter is pressed
-  passwordField.onkeypress = () => {
+  passwordField.onkeypress = event => {
     const keyName = event.key;
     if (keyName === 'Enter') {
       let form = document.getElementById('Login');
@@ -13,19 +13,25 @@ export const initMtsLogin = () => {
     }
   };
 
-  //Prevent keyboard shortcuts to copy to clipboard
-  passwordField.onkeydown = () => {
-    if ((event.key === 'c' || event.key === 'x') && event.metaKey) {
+  // Disables the context menu 
+  passwordField.oncontextmenu = event => {
+    if(event.preventDefault !== undefined) event.preventDefault();
+    if(event.stopPropagation !== undefined) event.stopPropagation();
+  };
+
+  // Prevent keyboard shortcuts to copy to clipboard
+  passwordField.onkeydown = event => {
+    if ((event.key === 'c' || event.key === 'x') && (event.metaKey || event.ctrlKey)) {
       return false;
     }
   };
 
-  //Prevent mouse click from copying/cutting to clipboard
-  passwordField.oncopy = () => {
-    return false;
-  };
+  // Prevents drag interaction to another field from passwordField
+  passwordField.ondragstart = event => event.preventDefault();
+  
+  // Prevents copying (including via shortcut) to clipboard from password field
+  passwordField.oncopy = event => false;
 
-  passwordField.oncut = () => {
-    return false;
-  };
+  // Prevents copying (including via shortcut) to clipboard from password field
+  passwordField.oncut = event => false;
 };

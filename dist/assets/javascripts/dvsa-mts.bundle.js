55a758c96ad0457514657584262f375abe14917c
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-01-29 18:06:26 +0000

    Built files for mov dev - defect picker

--- dist/assets/javascripts/dvsa-mts.bundle.js
+++ dist/assets/javascripts/dvsa-mts.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts
  *   Version: 1.3.1
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: January 28th 2019, 4:03:23 pm
+ *   Timestamp: January 30th 2019, 9:14:44 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */

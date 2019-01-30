1326e62fa68af02cf78592c9673f290d8d43118c
Martin di Martino-Marriott <hello@martindm.uk>
2019-01-30 10:14:21 +0000

    Update moth paths (#224)
    Updated path for MOTH stylesheet

--- dist/assets/javascripts/dvsa-mts.bundle.js
+++ dist/assets/javascripts/dvsa-mts.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts
  *   Version: 1.3.1
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: January 28th 2019, 3:15:48 pm
+ *   Timestamp: January 28th 2019, 4:03:23 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

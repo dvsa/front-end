0bae0a7a3f980b6171d5c4a47144c2e66b557940
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-02-01 17:02:01 +0000

    Breadcrumb tweaks for Browse defects

--- dist/assets/javascripts/dvsa-mts.bundle.js
+++ dist/assets/javascripts/dvsa-mts.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts
  *   Version: 1.3.3
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 1st 2019, 4:55:00 pm
+ *   Timestamp: February 4th 2019, 9:24:53 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

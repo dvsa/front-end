df16cde7354e6592f27e934fdbaa07acbb21a2be
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-02-12 10:28:34 +0000

    Built asset files for v1.3.8

--- dist/assets/javascripts/dvsa.bundle.js
+++ dist/assets/javascripts/dvsa.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa
  *   Version: 1.3.8
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 12th 2019, 10:21:21 am
+ *   Timestamp: February 12th 2019, 10:27:34 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */

dc2c19479bf48aae4e463ec6f349a10409039282
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-02-26 14:05:24 +0000

    Extend Gov uk tag for MTS Advisories

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/development
  *   Version: 1.3.9
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 26th 2019, 12:59:25 pm
+ *   Timestamp: March 6th 2019, 1:30:53 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

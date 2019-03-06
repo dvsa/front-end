e59d16cea0b3925a93bad5f7a0b062017741ec35
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-06 16:31:11 +0000

    * Button padding * govuk-form overrides for errors

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/development
  *   Version: 1.3.9
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 6th 2019, 1:30:53 pm
+ *   Timestamp: March 6th 2019, 4:31:40 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

5792b596b212b8735b876b487819595ee5c9fd2d
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-08 12:17:26 +0000

    Built prod files

--- dist/assets/javascripts/development.bundle.js
+++ dist/assets/javascripts/development.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/development
  *   Version: 1.3.9
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 8th 2019, 10:11:15 am
+ *   Timestamp: March 8th 2019, 12:15:31 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

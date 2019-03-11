b242ea173ada183e67a51f714eab164547b6ebfe
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-11 16:12:23 +0000

    Built files for Swimlane

--- dist/assets/javascripts/dvsa-mts.bundle.js
+++ dist/assets/javascripts/dvsa-mts.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts
  *   Version: 1.3.9
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 11th 2019, 12:14:50 pm
+ *   Timestamp: March 11th 2019, 4:07:14 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

adb125915bad177cad7f9736e494531bdc195f22
martindm <hello@martindm.uk>
2019-03-12 22:16:49 +0000

    Built files for v1.3.91

--- dist/assets/javascripts/dvsa.bundle.js
+++ dist/assets/javascripts/dvsa.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa
  *   Version: 1.3.9
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 13th 2019, 2:33:38 pm
+ *   Timestamp: March 12th 2019, 10:13:04 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

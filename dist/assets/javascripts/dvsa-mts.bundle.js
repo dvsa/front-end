aa1477cef0e2721fd80a822294a7459a09ad773b
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-18 13:51:44 +0000

    Built prod for Heroku

--- dist/assets/javascripts/dvsa-mts.bundle.js
+++ dist/assets/javascripts/dvsa-mts.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts
  *   Version: 1.3.10
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 18th 2019, 12:36:43 pm
+ *   Timestamp: March 18th 2019, 1:50:50 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

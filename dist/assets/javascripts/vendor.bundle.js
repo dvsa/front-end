d0b158968e91e28a8bcaf2b170514b82cb43c0f7
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-18 12:23:24 +0000

    Built files for Heroku demo

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.10
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 12th 2019, 2:14:12 pm
+ *   Timestamp: March 18th 2019, 12:21:20 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

d21669ca7c831b7bc5bf86c4d70c1baf257eceea
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-20 17:19:11 +0000

    Built files for MOT release 1.3.11 - Post Responsive UI

--- dist/assets/javascripts/dvsa-mts-legacy.bundle.js
+++ dist/assets/javascripts/dvsa-mts-legacy.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts-legacy
  *   Version: 1.3.10
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 12th 2019, 2:14:12 pm
+ *   Timestamp: March 20th 2019, 5:18:03 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

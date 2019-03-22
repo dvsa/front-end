ec0aee03ef94f89cd23f6c37348c78269a85ce62
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-03-22 01:09:06 +0000

    Built production files for Heroku

--- dist/assets/stylesheets/development.css
+++ dist/assets/stylesheets/development.css
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/development
  *   Version: 1.3.10
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 21st 2019, 3:25:28 pm
+ *   Timestamp: March 22nd 2019, 1:08:14 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */

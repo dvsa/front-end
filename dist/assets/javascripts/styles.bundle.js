e2525b2c46930dfc7c901ee55bcdfd30247d1ecb
Carl Fagg <carlf@kainos.com>
2019-05-29 14:11:52 +0100

    BL-9888 Regenerate assets

---
+++ dist/assets/javascripts/styles.bundle.js
@@ -0,0 +1,10 @@
+/*!
+ * 
+ *   Name: dvsa-front-end/styles
+ *   Version: 1.3.15
+ *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
+ *   Timestamp: May 29th 2019, 2:04:11 pm
+ *   Source: https://github.com/dvsa/front-end
+ * 
+ */
+webpackJsonp([5],{363:function(n,c){}},[363]);

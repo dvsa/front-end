e8dcac52c91430933f3fd7b142c52a5acf894486
Carl Fagg <carlf@kainos.com>
2019-06-13 11:37:17 +0100

    BL-9733: Building production assets

--- dist/assets/javascripts/dvsa-mts-legacy.bundle.js
+++ dist/assets/javascripts/dvsa-mts-legacy.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-mts-legacy
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 13th 2019, 11:20:08 am
+ *   Timestamp: June 13th 2019, 11:32:42 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */

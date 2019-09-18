50d03354194971394320940978d92f6f0f93e7c4
Helen Joy <helen.joy@bjss.com>
2019-09-18 16:20:41 +0100

    rebuild assets

--- dist/assets/javascripts/dvsa-manuals.bundle.js
+++ dist/assets/javascripts/dvsa-manuals.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/dvsa-manuals
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 13th 2019, 11:32:42 am
+ *   Timestamp: September 18th 2019, 4:15:52 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */

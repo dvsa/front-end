f3696b048c647561d681fc0e7f33b2f201844c15
Igor Stimoli <igor.stimoli@bjss.com>
2020-02-17 11:06:00 +0000

    BL-10710: remove old CookieMessage

--- src/assets/js/shared/gds/modules/index.js
+++ src/assets/js/shared/gds/modules/index.js
@@ -1,4 +1,3 @@
-import { CookieMessage } from './cookie-message';
 import { HeaderMenu } from './header-menu';
 import { LinksWithRole } from './links-with-role';
 import { ShowHideContent } from './show-hide-content';
@@ -6,7 +5,6 @@ import { SafarFontFix } from './safar-font-fix';
 import { SelectionButtons } from './selection-buttons';
 
 export const initModules = () => {
-  new CookieMessage();
   new HeaderMenu();
   new LinksWithRole();
   new ShowHideContent();
